9bbf1c73e5e7e10ec64d5e9db35deaaa64ea9317
Lucas Ilstedt <lucasilstedt@gmail.com>
2023-03-12 13:14:33 +0100

    Started work on puzzles

---
+++ Assets/Scripts/PzlNOT.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class PzlNOT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1 nere on
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // inputs för övre är output från nedre
+    }
+}
