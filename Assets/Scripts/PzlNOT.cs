f2e5160950e917bde2a4b64f3fea9d48955e1069
Lucas Ilstedt <lucasilstedt@gmail.com>
2023-03-13 08:28:57 +0100

    Hårdkodning go BRR

--- Assets/Scripts/PzlNOT.cs
+++ Assets/Scripts/PzlNOT.cs
@@ -6,6 +6,9 @@ using UnityEngine.XR;
 
 public class PzlNOT : MonoBehaviour
 {
+    public GateNOT outp;
+    public LineRenderer lineA, lineB;
+    public Dropdown A, B;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,48 @@ public class PzlNOT : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // inputs för övre är output från nedre
+        switch (A.value)
+        {
+            case 0:
+                // Debug.Log("LineA def");
+                lineA.startColor = Color.red;
+                lineA.endColor = Color.red;
+                break;
+            case 1:
+                // Debug.Log("LineA red");
+                lineA.startColor= Color.red;
+                lineA.endColor= Color.red;
+                break;
+            case 2:
+                // Debug.Log("LineA green");
+                lineA.startColor= Color.green;
+                lineA.endColor= Color.green;
+                break;
+        }
+        switch (B.value)
+        {
+            case 0:
+                // Debug.Log("LineB def");
+                lineB.startColor = Color.red;
+                lineB.endColor = Color.red;
+                break;
+            case 1:
+                // Debug.Log("LineB green");
+                lineB.startColor = Color.green;
+                lineB.endColor = Color.green;
+                break;
+            case 2:
+                // Debug.Log("LineB red");
+                lineB.startColor = Color.red;
+                lineB.endColor = Color.red;
+                break;
+        }
+        if (A.value == 2 && B.value == 1)
+            outp.SetInputs("f");
+        else
+            outp.SetInputs("t");
     }
 }
