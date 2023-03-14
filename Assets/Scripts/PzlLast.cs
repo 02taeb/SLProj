3700abf1be98d86e253ddc263e076ae1865db667
Lucas Ilstedt <lucasilstedt@gmail.com>
2023-03-14 09:28:02 +0100

    Started on PzlLast

---
+++ Assets/Scripts/PzlLast.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class PzlLast : MonoBehaviour
+{
+    public GateAND outp;
+    public List<LineRenderer> lines = new List<LineRenderer>();
+    public List<Dropdown> components = new List<Dropdown>();
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
